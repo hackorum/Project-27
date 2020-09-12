f2aac24bd9b640f1c0f7e45ae6259e6a06b00855
Neil Sabde <70092545+hackorum@users.noreply.github.com>
2020-09-12 18:18:08 +0530

    Update Bobs.js

--- Bobs.js
+++ Bobs.js
@@ -2,7 +2,7 @@ class Bob {
     constructor(x, y, r) {
 
         var options = {
-            restitution: 1,
+            restitution: 1.2,
             friction: 0.5,
             density: 1.2,
             isStatic: false
